c6d93fd137bcda0e837cd53bf2e9c770b51addb8
Hbju <milou@ordonneau.com>
2020-04-26 17:14:22 +0200

    infobulls fully added

---
+++ Assets/CharInfobull.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using TMPro;
+
+public class CharInfobull : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI text;
+	[SerializeField] GameObject infobull;
+
+	bool isActivated = false;
+
+	private void Start() {
+		Character chara = GetComponent<Player>().GetCharacter();
+		StringBuilder sb = new StringBuilder();
+		sb.Append(chara.GetName() + ": \n");
+		sb.Append("Health: " + chara.GetHealth() + "\n"); // TODO add current hp
+		sb.Append("Movement: " + chara.GetRadius() + "\n");
+		text.text = sb.ToString();
+
+		infobull.SetActive(isActivated);
+	}
+
+	private void Update() {
+		if (isActivated && Input.GetMouseButtonUp(1))
+			Switch();
+	}
+
+	public void Switch() {
+		Debug.Log(infobull);
+		infobull.SetActive(isActivated = !isActivated);
+	}
+}
