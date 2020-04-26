e219896ca4b40503150f824e72bfe8165c572327
Hbju <milou@ordonneau.com>
2020-04-26 14:19:05 +0200

    added everything that was not there (sprites, Character.cs, QuestLogUI.cs)

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character {
+	string name;
+
+	Sprite battleSprite;
+	int health;
+
+	int battleRadius;
+
+	public Character(string name, int health, int battleRadius) {
+		this.name = name;
+		this.battleSprite = Resources.Load<Sprite>("Sprites/Characters/" + name);
+		this.health = health;
+		this.battleRadius = battleRadius;
+	}
+
+	// getters
+	public string GetName() {
+		return name;
+	}
+
+	public int GetHealth() {
+		return health;
+	}
+
+	public int GetRadius() {
+		return battleRadius;
+
+	}
+
+	public Sprite GetSprite() {
+		return battleSprite;
+	}
+
+}
---
+++ Assets/Scripts/Events and Quests/QuestLogUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestLogUI : MonoBehaviour {
+	[SerializeField] TextMeshProUGUI text;
+	QuestLog log;
+
+	bool isActivated;
+
+	private void Awake() {
+		isActivated = false;
+	}
+
+	private void Start() {
+		log = FindObjectOfType<QuestLog>();
+
+		gameObject.SetActive(isActivated);
+	}
+
+	public void Switch() {
+		if (!isActivated) {
+			text.text = log.LogToString();
+			isActivated = true;
+		}
+		else 
+			isActivated = false;
+
+		gameObject.SetActive(isActivated);
+	}
+}
